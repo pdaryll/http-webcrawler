b84eea4b5184a2f3deec86380613f5f3621c238e
Daryll <77931825+pdaryll@users.noreply.github.com>
2023-11-20 17:22:23 +0800

    Finish crawlPage function
    Finished the crawlPage function and got it working with error handling

--- crawl.js
+++ crawl.js
@@ -1,27 +1,47 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentURL) {
-    console.log(`Actively crawling: ${currentURL}`)
+async function crawlPage(baseURL, currentURL, pages) {
     
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`Actively crawling: ${currentURL}`)
     try {
         const response = await fetch(currentURL)
 
         if (response.status > 399) {
             console.log(currentURL + " returned: " + response.status + " status")
-            return
+            return pages
         }
 
         const contentType = response.headers.get("content-type")
 
         if (!contentType.includes("text/html")) {
             console.log(`Site has Content Type of ${contentType}, must be text/html`)
-            return
+            return pages
         }
 
-        console.log(await response.text())
+        const htmlBody = await response.text()
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch (err) {
         console.log("Error: " + err.message)
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
